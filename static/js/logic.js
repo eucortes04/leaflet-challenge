0bbe6991d224792e146575311339b1e22c359339
EdgarCortes <eucortes04@gmail.com>
2021-06-17 21:57:09 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,6 @@
 // Store API enpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+//var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(response) {
@@ -10,41 +11,43 @@ d3.json(queryUrl).then(function(response) {
   
     function onEachFeature(featureData, layer) {
       layer.bindPopup("<h3>" + featureData.properties.place +
-        "</h3><hr><p>" + new Date(featureData.properties.time) + "</p>");
+        "</h3><hr><p>" + new Date(featureData.properties.time) + "</p><p>"+ featureData.properties.mag+"</p>");
+    }
+
+    function colorDepth(earthquakeDepth){
+        if(earthquakeDepth > 90){return "#FF0000";}
+        else if(earthquakeDepth > 70){return"#F96000"}
+        else if(earthquakeDepth > 50){return"#e59100"}
+        else if(earthquakeDepth > 30){return"#c4ba00"}
+        else if(earthquakeDepth > 10){return"#91df00"}
+        else{return "#00FF00";}
     }
   
   
     var earthquakes = L.geoJSON(features, {
       onEachFeature: onEachFeature,
-  
-      pointToLayer: (featureData, latlng) => 
-      {
-        console.log(featureData)
-        // return L.marker(latlng)
-        
+  //CHANGING SHIT HERE TO GET MARKERS WORKING
+      pointToLayer: function (featureData, latlng) {
         return L.circle(latlng,
-          {radius: featureData.properties.mag*10000}
+          {radius: featureData.properties.mag*100000,
+           color: "#000000",
+           weight: .3,
+           fillColor: colorDepth(featureData.geometry.coordinates[2]),
+           },
           )
-    }
+        }
     });
-  
-    
-    // var earthquakes = L.geoJSON(features, {
-    //   onEachFeature: onEachFeature,
-  
-    //   pointToLayer: (featureData, latlng) => 
-    //   {
-    //     console.log(featureData)
-    //  
-        
-    //     return L.circle(latlng,
-    //       {radius: featureData.properties.mag*10000}
-    //       ).bindPopup("<h3>" + featureData.properties.place +
-    //       "</h3><hr><p>" + new Date(featureData.properties.time) + "</p>");
-    // }
-    // });
-  
-  
+    //LEGEND FROM CLOROPLETH ACTIVITY
+    var legend = L.control({position:"bottomright"});
+    legend.onAdd = function(){
+        var div = L.DomUtil.create("div", "info legend");
+        var labels = ["0-10","10-30","30-40","40-50","50-70","90+"];
+        var colors = ["#00FF00","#91df00","#c4ba00","#e59100","#F96000","#FF0000"];
+        for (var i = 0; i < colors.length; i++){
+            div.innerHTML += "<i class='circle' style='background:"+colors[i]+"></i>"+ labels[i];
+        }
+        return div;
+    }
     
     // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -77,9 +80,9 @@ d3.json(queryUrl).then(function(response) {
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
       center: [
-        37.09, -95.71
+        40.247664005220656, -10.401832477641683
       ],
-      zoom: 5,
+      zoom: 2,
       layers: [streetmap, earthquakes]
     });
   
@@ -89,11 +92,5 @@ d3.json(queryUrl).then(function(response) {
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(myMap);
-  
-  
-  
-  
-  
-  
   });
   
